a939ff529e5e79f5369d97384777cb202e9b385f
vanessa moultrie <imvanessa16@gmail.com>
2023-11-28 22:26:48 -0500

    get route for * completed

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app.get('/feedback', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
 );
 
-// Wildcard route to direct users to a 404 page
+// Wildcard route to direct users to index.html
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
 app.listen(PORT, () =>
