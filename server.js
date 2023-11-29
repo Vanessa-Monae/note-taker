d69d335ca54f4826393d273586f3fd8fa0c3036b
vanessa moultrie <imvanessa16@gmail.com>
2023-11-28 22:24:37 -0500

    get route for /api/notes completed

--- server.js
+++ server.js
@@ -16,9 +16,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-// GET Route for / notes
+// GET Route for /notes
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for /api/notes
+app.get('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/db/db.json'))
 );
 
 // GET Route for feedback page
